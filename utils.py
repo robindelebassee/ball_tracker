1cbdc544c7e82a6d7f6bd47d5d65ea775ab648aa
romainf28 <=>
2023-03-27 13:37:17 +0200

    added ball detection using morphological criteria

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 
 
 def stabilize(frame, prev_gray, curr_gray, warp_matrix, warp_mode, termination_criteria):
@@ -31,9 +32,48 @@ def morphological_segmentation(fgmask, kernel):
     new_fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
     # apply canny edge detection on the foreground mask
-    edges = cv2.Canny(new_fgmask, 50, 190)
+    edges = cv2.Canny(new_fgmask, 50, 190, 3)
 
     # thresholding resulting image to keep only edges
     _, new_fgmask = cv2.threshold(edges, 127, 255, 0)
 
-    return fgmask
+    return new_fgmask
+
+
+def circularity_filter(contour):
+    # compute the area of the contour
+    area = cv2.contourArea(contour)
+
+    # compute the perimeter of the contour
+    perimeter = cv2.arcLength(contour, True)
+
+    circularity = 0
+
+    # check if perimeter is not null
+    if perimeter != 0:
+
+        # compute the ciruclarity of the contour
+        circularity = 4*math.pi*area/perimeter**2
+
+    return circularity >= 0.3 and circularity <= 0.8
+
+
+def eccentricity_filter(contour):
+    # extract the bounding rectangle coordinates and size
+    x, y, w, h = cv2.boundingRect(contour)
+
+    eccentricity = 1
+
+    # check that the width of the blob is not null
+    if w != 0:
+        # compute eccentricity
+        eccentricity = h/w
+
+    return eccentricity <= 0.7
+
+
+def size_filter(contour, min_area, max_area):
+
+    area = cv2.contourArea(contour)
+
+    return area > min_area and area < max_area
